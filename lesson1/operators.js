135b8105413755bc2e1162ad98f1e0ba54903b98
Tiffany Han <hantiffany@gmail.com>
2017-05-26 21:27:44 -0700

    finish lesson1

---
+++ lesson1/operators.js
@@ -0,0 +1,35 @@
+// var apples = 3;
+// var bananas = 5;
+// bananas = '3';
+
+// if (apples == bananas) {
+//   console.log('They\'re Equal!!');
+// }
+
+// apples = 3;
+// bananas = 3;
+// var areEqual = apples === bananas;
+// console.log(areEqual);
+
+// apples = 3;
+// bananas = 1;
+// var eitherOr = bananas || apples;
+// console.log(eitherOr);
+
+// if (apples === bananas) {
+//   console.log('They\'re strictly Equal!!');
+// } else {
+//     if (apples == bananas) {
+//       console.log('They\'re the same value but different types');
+//     }
+//     else {
+//       console.log('They\'re NOT strictly Equal!!');
+//     }
+// }
+
+// var lastName;
+// lastName = 'Han';
+// var familyMessage;
+
+// familyMessage = lastName === 'Han' ? 'Your\'re part of the family!' : 'You\'re not family';
+// console.log(familyMessage);
---
+++ lesson1/type.js
@@ -0,0 +1,37 @@
+// var x = '13';
+// var y = 9;
+
+// console.log(parseInt(x, 10) + y);
+
+// var x = '13';
+// var y = 9;
+
+// console.log(x * y); // will be a number
+
+// var npa = 212;
+// var nxx = 555;
+// var num = 1212;
+
+// console.log(npa.toString() + nxx.toString() + num.toString());
+
+// var npa = 212;
+// var nxx = 555;
+// var num = 1212;
+
+// console.log(String(npa) + String(nxx) + String(num));
+
+// var bool = true;
+// var arr = [1, 2, 3];
+
+// console.log(bool.toString());
+// console.log(arr.toString());
+
+// var npa = 212;
+// var nxx = 555;
+// var num = 1212;
+
+// npa += '';
+// nxx += '';
+// num += '';
+
+// console.log(npa + nxx + num);
