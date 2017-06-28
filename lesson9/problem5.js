9916364a61c4ef101b91a210eca1d96de1216254
Tiffany Han <hantiffany@gmail.com>
2017-06-28 12:27:38 -0700

    finish lesson9

---
+++ lesson9/problem5.js
@@ -0,0 +1,35 @@
+// function takes in a string message and a number of rails (integer)
+// output is the encoded message in rails fence cipher
+
+// should be case-insensitive
+// input string can contain other chars, like whitespace
+
+function encode(string, spots) {
+
+}
+
+// 'Hi Everyone', 2
+
+// H.E.E.Y.N.
+// .I.V.R.O.E
+
+// 'HEEYNIVROE'
+
+// 'Hi Everyone', 3
+
+// H...E...N.
+// .I.V.R.O.E
+// ..E...Y...
+
+// 'HENIVROEEY'
+
+// 'Hi Everyone', 4
+
+// H.....Y...
+// .I...R.O..
+// ..E.E...N.
+// ...V.....E
+
+// 'HYIROEENVE'
+
+// iterate through all rows at the same time
